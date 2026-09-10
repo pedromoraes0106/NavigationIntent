66fc4858dbfb02c4af4bcb4137e957d1b1d38236
pedromoraes0106 <pedrocamoraes05@gmail.com>
2026-09-09 22:31:42 -0300

    opa

---
+++ app/src/main/java/br/edu/ifsp/scl/sc304775x/navigationintent/MainViewModel.kt
@@ -0,0 +1,27 @@
+package br.edu.ifsp.scl.sc304775x.navigationintent
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MainViewModel(val savedStateHandle: SavedStateHandle): ViewModel() {
+    // var parameter by mutableStateOf("")
+    //    private set
+
+
+    private companion object{
+        const val PARAMETER_KEY = "parameter_key"
+    }
+
+    private val _parameterState = MutableStateFlow("")
+    val parameterState: StateFlow<String> = _parameterState.asStateFlow()
+
+    fun updateParameter(value: String){
+        _parameterState.value = value
+        savedStateHandle[PARAMETER_KEY] = value
+        // parameter = value
+    }
+
+}
